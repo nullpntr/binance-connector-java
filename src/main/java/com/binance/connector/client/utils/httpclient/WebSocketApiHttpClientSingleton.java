58edb28e67bb07beeda0f9cd5a4ca9295ca1f512
PeTRoS <petrenko.sergiy@gmail.com>
2023-08-11 19:28:58 +0300

    Added ping interval for okhttp client

--- src/main/java/com/binance/connector/client/utils/httpclient/WebSocketApiHttpClientSingleton.java
+++ src/main/java/com/binance/connector/client/utils/httpclient/WebSocketApiHttpClientSingleton.java
@@ -2,8 +2,11 @@ package com.binance.connector.client.utils.httpclient;
 
 import okhttp3.OkHttpClient;
 
+import java.util.concurrent.TimeUnit;
+
 public final class WebSocketApiHttpClientSingleton {
-    private static final OkHttpClient httpClient = new OkHttpClient();
+    private static final int PING_INTERVAL = 2;
+    private static final OkHttpClient httpClient = new OkHttpClient().newBuilder().pingInterval(PING_INTERVAL, TimeUnit.MINUTES).build();
 
     private WebSocketApiHttpClientSingleton() {
     }
