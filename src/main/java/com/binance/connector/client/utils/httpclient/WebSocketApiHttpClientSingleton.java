cd519de57c1163de8a717ede6aeb31f9d0b2d911
PeTRoS <petrenko.sergiy@gmail.com>
2023-08-29 10:22:06 +0300

    Modified WS ping interval

--- src/main/java/com/binance/connector/client/utils/httpclient/WebSocketApiHttpClientSingleton.java
+++ src/main/java/com/binance/connector/client/utils/httpclient/WebSocketApiHttpClientSingleton.java
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient;
 import java.util.concurrent.TimeUnit;
 
 public final class WebSocketApiHttpClientSingleton {
-    private static final int PING_INTERVAL = 2;
+    private static final int PING_INTERVAL = 1;
     private static final OkHttpClient httpClient = new OkHttpClient().newBuilder().pingInterval(PING_INTERVAL, TimeUnit.MINUTES).build();
 
     private WebSocketApiHttpClientSingleton() {
