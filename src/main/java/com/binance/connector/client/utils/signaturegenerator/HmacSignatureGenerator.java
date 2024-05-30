888c7b9970f186c51875422954ebc3e26e76ada2
petros <petrenko.sergiy@gmail.com>
2024-05-30 20:22:07 +0200

    Improved performance of HmacSignatureGenerator

--- src/main/java/com/binance/connector/client/utils/signaturegenerator/HmacSignatureGenerator.java
+++ src/main/java/com/binance/connector/client/utils/signaturegenerator/HmacSignatureGenerator.java
@@ -1,30 +1,32 @@
 package com.binance.connector.client.utils.signaturegenerator;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Hex;
 
-import com.binance.connector.client.utils.ParameterChecker;
-
 public final class HmacSignatureGenerator implements SignatureGenerator {
-    private static final String HMAC_SHA256 = "HmacSHA256";
-    private String apiSecret;
 
-    public HmacSignatureGenerator(String apiSecret) {
-        ParameterChecker.checkParameterType(apiSecret, String.class, "apiSecret");
-        this.apiSecret = apiSecret;
-    }
+  private static final String HMAC_SHA256 = "HmacSHA256";
 
-    public String getSignature(String data) {
-        byte[] hmacSha256;
-        try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(apiSecret.getBytes(), HMAC_SHA256);
-            Mac mac = Mac.getInstance(HMAC_SHA256);
-            mac.init(secretKeySpec);
-            hmacSha256 = mac.doFinal(data.getBytes());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to calculate hmac-sha256", e);
-        }
-        return Hex.encodeHexString(hmacSha256);
+  private final Mac mac;
+
+  public HmacSignatureGenerator(String apiSecret) {
+    try {
+      SecretKeySpec secretKeySpec = new SecretKeySpec(apiSecret.getBytes(), HMAC_SHA256);
+      mac = Mac.getInstance(HMAC_SHA256);
+      mac.init(secretKeySpec);
+    } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  public String getSignature(String data) {
+    try {
+      return Hex.encodeHexString(mac.doFinal(data.getBytes()));
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to calculate hmac-sha256", e);
+    }
+  }
 }
